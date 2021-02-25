1381d8d8f849f9b53ba9168cc8d830bf5556e764
unknown <thomas.vanvelthooven@gmail.com>
2021-02-25 16:11:18 +0100

    added the base of favorites panel rework.
    backup commit.

---
+++ src/com/blackdragon2447/AAM/gui/components/JNumberedPanel.java
@@ -0,0 +1,62 @@
+package com.blackdragon2447.AAM.gui.components;
+
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+import javax.accessibility.Accessible;
+import javax.swing.JPanel;
+
+public class JNumberedPanel extends JPanel implements Accessible{
+
+	private static final long serialVersionUID = 1L;
+	private int number;
+	
+	public JNumberedPanel(LayoutManager layout, boolean isDoubleBuffered) {
+        super(layout, isDoubleBuffered);
+    }
+
+    public JNumberedPanel(LayoutManager layout) {
+        this(layout, true);
+    }
+
+    public JNumberedPanel(boolean isDoubleBuffered) {
+        this(new FlowLayout(), isDoubleBuffered);
+    }
+
+    public JNumberedPanel() {
+        this(true);
+    }
+    
+
+	public JNumberedPanel(LayoutManager layout, boolean isDoubleBuffered, int Number) {
+        super(layout, isDoubleBuffered);
+        this.number = Number;
+    }
+
+    public JNumberedPanel(LayoutManager layout, int Number) {
+        this(layout, true);
+        this.number = Number;
+    }
+
+    public JNumberedPanel(boolean isDoubleBuffered, int Number) {
+        this(new FlowLayout(), isDoubleBuffered);
+        this.number = Number;
+    }
+
+    public JNumberedPanel(int Number) {
+        this(true);
+        this.number = Number;
+    }
+    
+    public int getNumber() {
+		return number;
+	}
+    
+    public void setNumber(int number) {
+		this.number = number;
+	}
+
+    
+	
+	
+}
